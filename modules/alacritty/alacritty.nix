113fa6046b163a561eeadd02b48a5fdeddbfc845
Juri Furer <juri.furer@dvbern.ch>
2026-06-03 12:07:35 +0200

    fix: remove args from tmux on start

--- modules/alacritty/alacritty.nix
+++ modules/alacritty/alacritty.nix
@@ -20,13 +20,6 @@
 
       settings = {
         terminal.shell = {
-          args = [
-            "new-session"
-            "-A"
-            "-D"
-            "-s"
-            "main"
-          ];
           program = "${pkgs.tmux}/bin/tmux";
         };
 
